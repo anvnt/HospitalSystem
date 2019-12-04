57340ac6ef80ea26801459c61def774f435d191a
anvnt <54789429+anvnt@users.noreply.github.com>
2019-12-04 23:23:21 +0700

    bottom navigation bar - đã chạy được

--- app/src/main/java/com/an/hospitalsystem/BookingFragment.java
+++ app/src/main/java/com/an/hospitalsystem/BookingFragment.java
@@ -1,31 +1,18 @@
 package com.an.hospitalsystem;
 
-
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class BookingFragment extends Fragment {
-
-
-    public BookingFragment() {
-        // Required empty public constructor
-    }
-
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_booking,container,false);
     }
-
 }
--- app/src/main/java/com/an/hospitalsystem/MainActivity.java
+++ app/src/main/java/com/an/hospitalsystem/MainActivity.java
@@ -1,23 +1,51 @@
 package com.an.hospitalsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnHome=findViewById(R.id.button);
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new NewsFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.navigation_news:
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case R.id.navigation_consultant:
+                            selectedFragment = new ConsultantFragment();
+                            break;
+                        case R.id.navigation_booking:
+                            selectedFragment = new BookingFragment();
+                            break;
+                        case R.id.navigation_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
 
-    public void moManHinhHome(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-    }
+                }
+
+
+            };
 }
