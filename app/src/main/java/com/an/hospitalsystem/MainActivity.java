77b9e66aa416408049e8d8fca35aab83db3bf778
anvnt <54789429+anvnt@users.noreply.github.com>
2019-12-04 21:09:21 +0700

    Bottom navigation bar và các fragments ver1

--- app/src/main/java/com/an/hospitalsystem/MainActivity.java
+++ app/src/main/java/com/an/hospitalsystem/MainActivity.java
@@ -2,13 +2,22 @@ package com.an.hospitalsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnHome=findViewById(R.id.button);
+    }
+
+    public void moManHinhHome(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
