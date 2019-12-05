0e4e6f9bba22f9a1a59aac83aa66fe0929dbe156
anvnt <54789429+anvnt@users.noreply.github.com>
2019-12-05 23:39:16 +0700

    Màn hình booking

--- app/src/main/java/com/an/hospitalsystem/BookingFragment.java
+++ app/src/main/java/com/an/hospitalsystem/BookingFragment.java
@@ -1,18 +1,132 @@
 package com.an.hospitalsystem;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class BookingFragment extends Fragment {
+    View view;
+    Spinner spSex;
+    ArrayAdapter <String>adapter;
+    EditText edtFullName, edtDOB, edtPhone, edtEmail, edtAddress,edtDate,edtTime ;
+    Calendar calendarDate=Calendar.getInstance();
+    SimpleDateFormat sdfDate = new SimpleDateFormat("dd/MM/yyyy");
+    Calendar calendarTime=Calendar.getInstance();
+    SimpleDateFormat sdfTime=new SimpleDateFormat("HH:mm");
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_booking,container,false);
+        view = inflater.inflate(R.layout.fragment_booking,container,false);
+            addControls(); 
+            addEvents();
+            return view ;
+    }
+
+    private void addEvents() {
+        edtDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moManHinhDatePickerDialog();
+            }
+        });
+        edtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moManHinhDatePickerDialogBooking();
+            }
+        });
+        edtTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moManHInhTimePickerDialog();
+            }
+        });
+    }
+
+    private void moManHInhTimePickerDialog() {
+        TimePickerDialog.OnTimeSetListener callBack = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendarTime.set(Calendar.HOUR,hourOfDay);
+                calendarTime.set(Calendar.MINUTE,minute);
+                edtTime.setText(sdfTime.format(calendarTime.getTime()));
+            }
+        };
+        TimePickerDialog dialog= new TimePickerDialog(
+                view.getContext(),
+                callBack,
+                calendarTime.get(Calendar.HOUR),
+                calendarTime.get(Calendar.MINUTE),
+                true
+        );
+        dialog.show();
+    }
+
+
+    private void moManHinhDatePickerDialog() {
+        DatePickerDialog.OnDateSetListener callBack = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendarDate.set(Calendar.YEAR,year);
+                calendarDate.set(Calendar.MONTH,month);
+                calendarDate.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                edtDOB.setText(sdfDate.format(calendarDate.getTime()));
+
+            }
+        };
+        DatePickerDialog dialog=new DatePickerDialog(
+                view.getContext(),
+                callBack,
+                calendarDate.get(Calendar.YEAR),
+                calendarDate.get(Calendar.MONTH),
+                calendarDate.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    private void moManHinhDatePickerDialogBooking() {
+        DatePickerDialog.OnDateSetListener callBack = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendarDate.set(Calendar.YEAR,year);
+                calendarDate.set(Calendar.MONTH,month);
+                calendarDate.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                edtDate.setText(sdfDate.format(calendarDate.getTime()));
+
+            }
+        };
+        DatePickerDialog dialog=new DatePickerDialog(
+                view.getContext(),
+                callBack,
+                calendarDate.get(Calendar.YEAR),
+                calendarDate.get(Calendar.MONTH),
+                calendarDate.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+    }
+
+    private void addControls() {
+        spSex=view.findViewById(R.id.spSex);
+        adapter=new ArrayAdapter<String>(view.getContext(),android.R.layout.simple_spinner_item);
+        adapter.add("Male");
+        adapter.add("Female");
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spSex.setAdapter(adapter);
+        edtDOB=view.findViewById(R.id.edtDOB);
+        edtDate=view.findViewById(R.id.edtDate);
+        edtTime=view.findViewById(R.id.edtTime);
     }
 }
--- app/src/main/java/com/an/hospitalsystem/ConsultantResultFragment.java
+++ app/src/main/java/com/an/hospitalsystem/ConsultantResultFragment.java
@@ -4,15 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ConsultantResultFragment extends Fragment {
+    View view;
+    Button btnPrescription, btnPrediction, btnBooking;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_consultantresult,container,false);
+        view =  inflater.inflate(R.layout.fragment_consultantresult,container,false);
+        addControls();
+        addEvents();
+        return view;
     }
+
+    private void addControls() {
+        btnPrescription=view.findViewById(R.id.btnPrescription);
+        btnPrediction=view.findViewById(R.id.btnPrediction);
+        btnBooking=view.findViewById(R.id.btnBooking);
+    }
+    private void addEvents() {
+        btnBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mở màn hình Booking Fragment
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new BookingFragment());
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
 }
