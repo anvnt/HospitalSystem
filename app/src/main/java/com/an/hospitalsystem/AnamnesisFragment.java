deedba011d0b0c9d5a98628bff58620bc72dca73
anvnt <54789429+anvnt@users.noreply.github.com>
2019-12-06 21:39:31 +0700

    push tonight

--- app/src/main/java/com/an/hospitalsystem/AnamnesisFragment.java
+++ app/src/main/java/com/an/hospitalsystem/AnamnesisFragment.java
@@ -12,14 +12,14 @@ import androidx.fragment.app.Fragment;
 
 import com.an.adapter.NewsAdapter;
 
-public class NewsFragment extends Fragment {
+public class AnamnesisFragment extends Fragment {
     ListView lvNews;
     NewsAdapter newsAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_news,container,false);
+        return inflater.inflate(R.layout.fragment_anamnesis,container,false);
 
     }
 
--- app/src/main/java/com/an/hospitalsystem/ProfileFragment.java
+++ app/src/main/java/com/an/hospitalsystem/ProfileFragment.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ProfileFragment extends Fragment {
+    TextView txtAnamnesis, txtFamilyContact, txtDrugAllergy, txtCard, txtSupportCenter, txtAboutUs;
+    View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profiles,container,false);
+        view=inflater.inflate(R.layout.fragment_profiles,container,false);
+        addControls();
+        addEvents();
+        return view;
+    }
+
+    private void addEvents() {
+        txtAnamnesis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity()
+                        .getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new AnamnesisFragment());
+                fragmentTransaction.commit();
+            }
+        });
+    }
+
+    private void addControls() {
+        txtAnamnesis=view.findViewById(R.id.txtAnamnesis);
     }
 }
