e474138504ef3ac6eb3136d3886082d07bb4b4e8
anvnt <54789429+anvnt@users.noreply.github.com>
2019-12-06 21:28:51 +0700

    thêm hình

---
+++ app/src/main/java/com/an/hospitalsystem/AnamnesisFragment.java
@@ -0,0 +1,26 @@
+package com.an.hospitalsystem;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.an.adapter.NewsAdapter;
+
+public class NewsFragment extends Fragment {
+    ListView lvNews;
+    NewsAdapter newsAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news,container,false);
+
+    }
+
+}
