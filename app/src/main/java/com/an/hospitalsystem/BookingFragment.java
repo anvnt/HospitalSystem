66c1a8361fc12e3bdf9294af39fa6f67b7626f60
anhnhatuel <52560500+anhnhatuel@users.noreply.github.com>
2019-12-07 16:01:32 +0700

    Add notification for booking button

--- app/src/main/java/com/an/hospitalsystem/BookingFragment.java
+++ app/src/main/java/com/an/hospitalsystem/BookingFragment.java
@@ -1,7 +1,13 @@
 package com.an.hospitalsystem;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +22,8 @@ import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -64,6 +72,7 @@ public class BookingFragment extends Fragment {
         btnBooking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                xuLyTaoNotification(v);
             }
         });
     }
@@ -140,4 +149,68 @@ public class BookingFragment extends Fragment {
         edtTime=view.findViewById(R.id.edtTime);
         btnBooking=view.findViewById(R.id.btnBooking);
     }
+
+    //Hàm tạo Channel cho notification
+    public static String createNotificationChannel(Context context) {
+
+        // NotificationChannels are required for Notifications on O (API 26) and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // The id of the channel.
+            String channelId = "Channel_id";
+
+            // The user-visible name of the channel.
+            CharSequence channelName = "Application_name";
+            // The user-visible description of the channel.
+            String channelDescription = "Application_name Alert";
+            int channelImportance = NotificationManager.IMPORTANCE_DEFAULT;
+            boolean channelEnableVibrate = true;
+            //            int channelLockscreenVisibility = Notification.;
+
+            // Initializes NotificationChannel.
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, channelImportance);
+            notificationChannel.setDescription(channelDescription);
+            notificationChannel.enableVibration(channelEnableVibrate);
+            //            notificationChannel.setLockscreenVisibility(channelLockscreenVisibility);
+
+            // Adds NotificationChannel to system. Attempting to create an existing notification
+            // channel with its original values performs no operation, so it's safe to perform the
+            // below sequence.
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            return channelId;
+        } else {
+            // Returns null for pre-O (26) devices.
+            return null;
+        }
+    }
+    public void xuLyTaoNotification(View view) {
+        //Tạo channel ID
+        String channel_id = createNotificationChannel(getActivity());
+
+        //Thêm action cho noti
+        Intent intent = new Intent(getActivity(),MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent, 0);
+
+        NotificationCompat.Builder Builder = new NotificationCompat.Builder(getActivity(), channel_id)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Congrats")
+                .setContentText("Your booking successfully !!! Thanks for choosing us.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        ;
+
+
+        //Show Noti
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
+        notificationManager.notify(113, Builder.build());
+
+    }
+
+
 }
